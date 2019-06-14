f16ec8990328312ca8bc4935012f71bc433769ef
yangjun@chinafintech.cn <yangjun@chinafintech.cn>
2019-06-14 15:22:44 +0800

    ddd

--- src/models/user.js
+++ src/models/user.js
@@ -1,8 +1,46 @@
 import {login, modifyPassword} from '../services/user';
 import { md5 } from '@/utils';
 import router from 'umi/router';
+import Cookies from 'js-cookie';
 import pathToRegexp from 'path-to-regexp';
 
+// 用户存储cookie区域， 用户实际信息存储在localStorage内，在cookie内保存一个key来对应storage里面的内容
+class UserCookieStore{
+    storePrefix='';
+    userMarkPrefix='@user_';
+    Storage=localStorage;
+    cookieKey='user-key';
+    constructor(storePrefix){
+        this.storePrefix = storePrefix;
+    }
+    set(data){
+        this.clear();
+        if(!data)return;
+        const storeKey = this.getRealKey() + Date.now();
+        this.Storage.setItem(storeKey, window.JSON.stringify(data));
+        Cookies.set(this.cookieKey,storeKey,{path:'/'});
+    }
+    get(){
+        const storeKey = Cookies.get(this.cookieKey);
+        if(!storeKey)return null;
+        let user = this.Storage.getItem(storeKey);
+        if (!user)return null;
+        return window.JSON.parse(user);
+    }
+    getRealKey(){
+        return this.storePrefix + this.userMarkPrefix;
+    }
+    clear(){
+        const regx = new RegExp('^('+this.getRealKey()+')');
+        for(let i=0;i<this.Storage.length;i++){
+            const key = this.Storage.key(i);
+            if(regx.test(key)){
+                this.Storage.removeItem(key);
+            }
+        }
+    }
+}
+const userCookieStore = new UserCookieStore('x-starter');
 const localUser = getUserFromStore();
 
 export default {
@@ -68,17 +106,16 @@ function storeUser(user) {
         user = {...user};
         delete user.routeMap;
         delete user.dnaMap;
-        if (typeof user === 'object') user = JSON.stringify(user);
-        sessionStorage.setItem('user', user);
+        userCookieStore.set(user);
     }else {
-        sessionStorage.removeItem('user');
+        userCookieStore.clear();
     }
 }
 
 function getUserFromStore() {
-    const user = sessionStorage.getItem('user');
+    const user = userCookieStore.get();
     if(!user)return null;
-    return extUserAuthMap(JSON.parse(user));
+    return extUserAuthMap(user);
 }
 
 function extUserAuthMap(user) {
