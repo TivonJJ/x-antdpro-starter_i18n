6851cc475c40fd4ef496c99291aefeb1f521b451
yangjun@chinafintech.cn <yangjun@chinafintech.cn>
2019-07-18 11:41:33 +0800

    修复404页面显示

--- src/router-interceptor/authorized/Authed.js
+++ src/router-interceptor/authorized/Authed.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import E403 from '@/pages/Exception/403';
-// import E404 from '@/pages/Exception/404';
+import E404 from '@/pages/Exception/404';
 import {getRouteByLink} from "@/utils";
 import {checkUserAuth} from "@/components/Authorized";
 
 export default ({ children, route, location }) => {
     if(!location.pathname || location.pathname === '/')return children;
     if(!getRouteByLink(route.routes,location.pathname)){
-        return children
+        return <E404/>
     }
     if(!checkUserAuth(location.pathname)){
         return <E403/>
